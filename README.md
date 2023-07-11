e1e18b330ae4f41d687df2bd6de37c85d00a7e54
SamuelWango <wangos478@gmail.com>
2023-07-11 08:40:56 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # code-challenge-2
+# PET GALLERY
+
+PET GALLERY is a website that showcases cute animals and allows users to like their favorite animals. Users can also leave comments on each animal's page.
+
+## Preview
+
+To see a live preview of the website, follow the steps below:
+
+1. Clone the repository: `git clone <repository-url>`
+2. Navigate to the project directory: `cd <project-directory>`
+3. Open the index.html file in a web browser.
+
+## Features
+
+- Navigation menu with links to Home, Profile, and Settings
+- "See all animals" button to render all animals on the page
+- Each animal's page includes an image, a like button, and a comment section
+- Users can like animals and leave comments
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- CSS
+
+## Getting Started
+
+To run the website locally, follow these steps:
+
+1. Clone the repository: `git clone <repository-url>`
+2. Navigate to the project directory: `cd <project-directory>`
+3. Open the index.html file in a web browser.
+
+## How It Works
+
+The website fetches data from a JSON server at http://localhost:3000/characters. When the "See all animals" button is clicked, the data is retrieved and each animal's information is rendered on the page.
+
+Users can click the "Like" button to like an animal. The button updates its content and style based on whether the user has liked the animal or not. The like count is incremented or decremented accordingly, and the server is updated with the modified data.
+
+Users can also leave comments on each animal's page. The comments are added dynamically to the comment section and can be deleted by clicking the "x" button.
+
+## Animal Data
+
+The animal data is stored in a JSON format. Here are a few examples of animals included in the data:
+
+- Mr. Cute
+- Mx. Monkey
+- Ms. Zebra
+- Dr. Lion
+- Mme. Panda
+
+Each animal includes details such as the name, image URL, and number of votes.
+
+## Contributing
+
+Contributions to the project are welcome! If you find any issues or have suggestions for improvement, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- index.js
+++ index.js
@@ -1,6 +1,156 @@
-
+//function is called to the render all animals button
 function renderPage(){
-fetch('http://localhost:3000/characters')
-.then((response)=>response.json())
-.then((json)=>post(json))
-}
+    //fetches data from json server
+    fetch('http://localhost:3000/characters')
+    .then((response)=>response.json())
+    .then((json)=>post(json))
+    }
+    
+    
+    //this function takes thee content from json renders it to the page
+    function post(characters){
+    
+        //iterates over every object in the json server
+        characters.forEach((character) =>{
+        
+        //create an image element for our cute animals
+        let content=document.getElementById('content')
+        let divContent=document.createElement('div')
+        divContent.className="post"
+        
+    
+        let img=document.createElement('img')
+        img.src = character.image
+        divContent.appendChild(img)
+    
+        //create a like button
+       var btn=document.createElement('button')
+       btn.id='likeButton'
+       btn.innerHTML = 'Like'
+    
+       divContent.appendChild(btn)
+       btn.addEventListener('click',function (){
+        let characterName = character.name
+        let individualVote = 0
+    
+        //Ensures that you can only vote once for each animal
+        if(individualVote === 0){
+             individualVote++
+             character.votes++
+             btn.innerHTML = `Liked!: ${character.votes}`
+             btn.style.backgroundColor = "red"
+             updateData()
+        }else if(individualVote> 0){
+            individualVote--
+            character.votes--
+            btn.innerHTML = `like: ${character.votes}`
+            btn.style.backgroundColor = "white"
+            btn.style.color = "black"
+            updateData()
+        }
+        
+        //logs the like count and the updated object array of characters 
+        console.log(`${characterName} LikeCount: ${character.votes}`)
+        characters.forEach((character)=> console.log(character) ) 
+         })
+    
+         function updateData(){
+          fetch(`http://localhost:3000/characters/${character.id}`, {
+            method: 'PATCH',
+            headers:{
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(characters)
+          })
+          .then(res => res.json())
+          .then(character => console.log(character))
+    
+         }
+    
+    
+        //create the comment section
+         let commentSection=document.createElement('div')
+         commentSection.className = 'commentSection'
+        // Create the form element
+        const form = document.createElement('form')
+        form.id = `${character.id}'s comment section `
+        form.action = ''
+        form.method = 'POST'
+    
+    
+    
+        // Create the input element
+        const input = document.createElement('input')
+        input.type = 'text'
+        input.id = 'new-comment'
+        input.name = 'new-comment'
+        input.placeholder = 'Add your comment...'
+    
+        form.addEventListener('submit', (event)=>{
+            event.preventDefault()
+            addComment(document.getElementById('new-comment').value)
+            form.reset()
+          })
+    
+          //append input to form
+        form.appendChild(input)
+    
+        //append form to  comment section
+        commentSection.appendChild(form)
+    
+        // Create the list container
+        const commentContainer = document.createElement('div')
+        commentContainer.id = 'comments'
+    
+        // Create the heading element
+        const heading = document.createElement('h2')
+        heading.textContent = 'Comments'
+    
+        // Create the unordered list
+        let commentList = document.createElement('ul')
+        commentList.id = 'individualComment'
+    
+        // Append the heading and task list to the list container
+        commentContainer.appendChild(heading)
+        commentContainer.appendChild(commentList)
+    
+        // Append the comment container to comment section
+        commentSection.appendChild(commentContainer)
+    
+    
+      
+    
+        //add a comment to the comment section
+        function addComment(comment){
+            let username=prompt("Enter username: ")
+    
+    
+    
+            let item = document.createElement('li')
+            item.innerHTML=`${username}: ${comment}  `
+          
+            let btn = document.createElement('button')
+            btn.textContent = 'x'
+            btn.addEventListener('click', deletesComment)
+          
+            item.appendChild(btn)
+            commentList.appendChild(item)
+          
+          }
+    
+          //deletes a comment from the comment section
+          function deletesComment(event){
+            event.target.parentNode.remove()
+          
+          }
+    
+          //Append the comment section and posts to the page
+         content.appendChild(divContent)
+         content.appendChild(commentSection)
+        })
+    
+    }
+    
+    
+    
+    
